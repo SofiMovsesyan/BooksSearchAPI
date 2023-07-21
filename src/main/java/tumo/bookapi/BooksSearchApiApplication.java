c590e9959df59061697bd61c2a11af52397fabfb
SofiMovsesyan <sofi170707@gmail.com>
2023-07-21 16:52:41 +0400

    create

---
+++ src/main/java/tumo/bookapi/BooksSearchApiApplication.java
@@ -0,0 +1,13 @@
+package tumo.bookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksSearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tumo/bookapi/BooksSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tumo.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksSearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
