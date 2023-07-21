269ce08025458afa24ad0c900c8af726a7b0eb86
SofiMovsesyan <sofi170707@gmail.com>
2023-07-21 17:05:51 +0400

    create.

--- src/main/java/tumo/bookapi/BooksSearchApiApplication.java
+++ src/main/java/tumo/bookapi/BooksSearchApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksSearchApiApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(BooksSearchApiApplication.class, args);
     }
