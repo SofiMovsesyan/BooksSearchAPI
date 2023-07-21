da9034116518fdf189a65120893cb53eb415a4d5
vivienhasratyan <vivien.hasratyan.y@tumo.org>
2023-07-21 17:55:08 +0400

    ServiceImpl

--- src/main/java/tumo/bookapi/implementation/services/BooksServiceImpl.java
+++ src/main/java/tumo/bookapi/implementation/services/BooksServiceImpl.java
@@ -1,4 +1,59 @@
 package tumo.bookapi.implementation.services;
 
-public class BooksServiceImpl {
+import jakarta.annotation.Nullable;
+import org.springframework.stereotype.Service;
+import tumo.bookapi.api.domain.Books;
+import tumo.bookapi.api.repositories.BooksRepository;
+import tumo.bookapi.api.services.BooksService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BooksServiceImpl implements BooksService {
+    private final BooksRepository booksRepository;
+
+    public BooksServiceImpl(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    @Override
+    public Books findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Books findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Books findByAuthor(String name) {
+        return null;
+    }
+
+    @Override
+    public Books findByGenre(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Books> findAll() {
+        return null;
+    }
+
+    @Override
+    public Books saveProduct(String name, String author, String description, String genre) {
+        return null;
+    }
+
+    @Override
+    public Books updateProduct(String name, String author, @Nullable String genre, @Nullable String description) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+
+    }
 }
